a0fb51a7d47461dc47f65764c101a20d11d1c7b1
NemanjaJoe <jessicaruixin@outlook.com>
2021-05-01 11:03:57 +0800

    update README and added some descriptions to quiz 11

--- programming/OOP-cpp/README.md
+++ programming/OOP-cpp/README.md
@@ -1,3 +1,9 @@
-this file is the practice code of Object-Oriented Programming with C++
+## what is this directory?
+
+This directory contains the practice code of Object-Oriented Programming with C++.
+
+For every quiz within chapters, you can look at the quiz description first, which is in the ~main.cpp file. 
+
+If you are a novice in C++, you should try it yourself with your own solutions. Don't worry, those quiz is not very complicated and it deserves your time.
 
 the textbook used in this directory: 面向对象C++程序设计，皮德常 著， 清华大学出版社. Object-Oriented programming with C++.
--- programming/OOP-cpp/chapter1/p11-main.cpp
+++ programming/OOP-cpp/chapter1/p11-main.cpp
@@ -1,3 +1,19 @@
+//************************************************************************
+//* program auther: NemanjaJoe
+//* date: April 31th, 2021
+//* chapter: 1
+//* quiz index: 11
+//* description: write a program to simulate cheque printer. user are required to input
+//*              date, name and the amount of money of this cheque.
+//*              then program should simulate printer to print the formative information of this cheque.
+//*              for insatance:
+//*                             date: April 31th, 2021
+//*                             name: nemanjajoe
+//*                             money: 898.32
+//* note: 1.the percision of the formative money is tow.
+//*       2.the amount of money is limited from 0 to 1000.
+//************************************************************************
+
 #include "p11-class.h"
 
 using namespace std;
